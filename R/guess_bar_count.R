86fd80bb44b46e1bb854615d06b123fbe3e92705
Adam Spannbauer <SpannbauerAdam@gmail.com>
2019-06-15 20:06:50 -0400

    namespace non base calls

--- R/guess_bar_count.R
+++ R/guess_bar_count.R
@@ -5,21 +5,21 @@ guess_bar_count = function(x, min_bar = 3, max_bar = 25) {
   fit_x = seq_along(x)
   fit_y = sort(x, decreasing = TRUE)
 
-  range_fit_x = c(head(fit_x, 1), tail(fit_x, 1))
-  range_fit_y = c(head(fit_y, 1), tail(fit_y, 1))
+  range_fit_x = c(utils::head(fit_x, 1), utils::tail(fit_x, 1))
+  range_fit_y = c(utils::head(fit_y, 1), utils::tail(fit_y, 1))
 
   fit_df = data.frame(x = range_fit_x, y = range_fit_y)
 
   # Creating straight line between the max values
-  fit = lm(y ~ x, data = fit_df)
+  fit = stats::lm(y ~ x, data = fit_df)
 
   # Distance from point to line
   distances = vapply(seq_along(x), function(i) {
     new_obs = data.frame(x = i)
-    pred = c(i, predict(fit, new_obs))
+    pred = c(i, stats::predict(fit, new_obs))
     obs = c(i, fit_y[i])
 
-    dist(rbind(pred, obs))[1]
+    stats::dist(rbind(pred, obs))[1]
   }, numeric(1))
 
   # Max distance point
